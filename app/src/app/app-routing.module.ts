eb439c7b2d1a22949eaef669c14aa65afeff2991
wachuuu <gab.wachowski15@gmail.com>
2022-04-28 22:23:53 +0200

    simple routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ShopComponent } from './components/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ShopComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { ShopComponent } from './components/shop/shop.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShopComponent,
     ProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
